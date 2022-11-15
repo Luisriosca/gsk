769fef6501ceb02bbdaeed8dbef4e9c8a791b916
luis.rios@legux.com <luis.rios.9815@gmail.com>
2022-11-14 21:59:18 -0600

    Adding some structure to the project. Starting with the styles. Creating the layout module for components only.

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { LayoutComponent } from './layout.component';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class LayoutModule { }
